2fdbd42627c1088899fcbbc7581f160bb80b29ea
PavelTrofimov <pavtrof342@gmail.com>
2017-04-10 21:36:58 +0500

    datepicker fix todo

--- src/js/apps/App/components/DatePicker/dateCreator.js
+++ src/js/apps/App/components/DatePicker/dateCreator.js
@@ -3,6 +3,8 @@ import {extendMoment} from 'moment-range';
 
 const moment = extendMoment(Moment);
 
+const insert = (arr, index, newItem) => [...arr.slice(0, index), newItem, ...arr.slice(index)];
+
 export default function getCalendar(year, month) {
   const startDate = moment([year, month]);
   const firstDay = moment(startDate).startOf('month');
@@ -30,14 +32,16 @@ export default function getCalendar(year, month) {
       firstWeekDay = moment([year, month]).week(week).day(1);
       lastWeekDay = moment([year, month]).week(week).day(7);
     }
-    /*todo не создается строка дат, если месяц начинается с вс 1-ого числа*/
-    /*    let test = parseInt(firstWeekDay.format('D'));
-     if(i === 0 && test === 2) {
-     let prevYear = month === 0 ? year - 1 : year;
-     let prevMonth = month === 0 ? 11 : month;
-     firstWeekDay = moment([prevYear, prevMonth]).week(week).day(1);
-     lastWeekDay = moment([prevYear, prevMonth]).week(week).day(7);
-     }*/
+     let numbOfDay = parseInt(firstWeekDay.format('D'));
+     if(i === 0 && numbOfDay === 2) {
+       weeks = insert(weeks, 1, week);
+       let prevYear = month === 0 ? year - 1 : year;
+       let prevMonth = month === 0 ? 11 : month;
+       let prevWeek = week === 1 ? 53 : week - 1;
+
+       firstWeekDay = moment([prevYear, prevMonth]).week(prevWeek).day(1);
+       lastWeekDay = moment([prevYear, prevMonth]).week(prevWeek).day(7);
+     }
     if (firstWeekDay.month() === month || lastWeekDay.month() === month) {
       let weekRange = moment.range(firstWeekDay, lastWeekDay);
       calendar.push(weekRange);
