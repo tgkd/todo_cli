80f390377095255ee88887a5a5a667c727aa5bb4
Trofimov Pavel <pavtrof342@gmail.com>
2017-04-25 10:40:12 +0500

    heroku run

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,7 +11,7 @@ gulp.task('default', ['dist'], () => {
   console.log('default task');
 });
 
-gulp.task('dist', ['stylus', 'assets', 'fonts'], () => {
+gulp.task('dist', ['stylus', 'js', 'assets', 'fonts'], () => {
   return gulp.src(['./src/html/*.html'])
     .pipe(gulp.dest('./public'))
 });
