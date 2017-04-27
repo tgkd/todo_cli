cfaa522add44bd53d20614ffac1adea79a071595
Trofimov Pavel <pavtrof342@gmail.com>
2017-04-27 16:19:02 +0500

    js minify

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -1,7 +1,21 @@
+const webpack = require('webpack');
+
 module.exports = {
   devtool: false,
   watch: false,
   output: {
     filename: '[chunkhash:12].js'
-  }
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false,
+        semicolons: true
+      },
+      sourceMap: true
+    })
+  ]
 };
