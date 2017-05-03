42bc6360cc02929b93ad071c456be08a3e436ef6
Trofimov Pavel <pavtrof342@gmail.com>
2017-05-03 17:04:04 +0500

    nav items padding

--- src/js/apps/App/components/Navigation/index.jsx
+++ src/js/apps/App/components/Navigation/index.jsx
@@ -22,7 +22,7 @@ export default class extends Component {
   render() {
     return (
       <div className="row center-md center-sm center-xs">
-        <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10 nav-links">
+        <div className="col-xs-10 col-sm-10 col-md-10 col-lg-10 nav-links nopadding">
           <div className="row between-xs between-sm between-md">
 
             <div className="nav-links__back">
